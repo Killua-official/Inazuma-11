b85c3d243654036e3b4aa1a20b8fa17a5e5fb7bb
Killua-official <120177140+Killua-official@users.noreply.github.com>
2023-10-29 21:02:59 +0600

    Add files via upload

---
+++ char.js
@@ -0,0 +1,14 @@
+// "jQuery" library is used to resize cards in "characters.html" file
+$(document).ready(function () {
+    let maxHeight = 0;
+    let $cards = $('.card');
+    $cards.each(function () {
+        let cardHeight = $(this).height();
+        if (cardHeight > maxHeight) {
+            maxHeight = cardHeight;
+        }
+    });
+    $cards.height(maxHeight);
+});
+
+
